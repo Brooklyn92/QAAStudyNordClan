461539e99af8d16e729b5838a1535803f202829b
Brooklyn92 <Tubanar173russia>
2022-05-20 16:46:04 +0400

    add plugins

--- src/test/java/testTracker/AuthTest.java
+++ src/test/java/testTracker/AuthTest.java
@@ -14,6 +14,7 @@ import java.time.Duration;
 import java.util.stream.Stream;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+
 public class AuthTest {
     private final By CHECK_TITLE = By.xpath("//h1[contains(@class,'title')]");
 
